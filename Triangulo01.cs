b99f8b6777b20bde4b5780810da5946a09f40f40
giovanny91espinal <giovanny.espinal@gmail.com>
2020-07-08 12:04:31 -0500

    Add files via upload

---
+++ Triangulo01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Ingrese X en radianes: ");
+            double x = double.Parse(Console.ReadLine());
+            double xGrados = x * (180.0 / Math.PI);
+            Console.WriteLine("El equivalente de X en grados es: " + xGrados);
+        }
+    }
+}
