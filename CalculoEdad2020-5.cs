b299ef2135c6edc6a9141cc0634aaabf9d25dba8
giovanny91espinal <giovanny.espinal@gmail.com>
2020-07-09 09:45:25 -0500

    Add files via upload

---
+++ CalculoEdad2020-5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Por favor ingrese su año de nacimiento: ");
+            int añoNacimiento = int.Parse(Console.ReadLine());
+            int añoActual = 2020;
+            int edad = añoActual - añoNacimiento;
+            Console.WriteLine("Su edad es: " + edad);
+            
+        }
+    }
+}
